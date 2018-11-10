ade895eec676a824b0f5c29c7e08beb3c95e51ff
blnicholson <blnicholson13@gmail.com>
2018-11-10 12:41:47 -0600

    restructured files

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express=require("express");
 const mongoose=require("mongoose");
 const routes=require("./routes");
 const app=express();
-const PORT= process.env.PORT || 3006;
+const PORT= process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+//Serve Static Assets
+app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.use(routes);
 
